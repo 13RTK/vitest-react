c6d245d5d7c09c8d242838df4150a93a16001e41
alex <907785012@qq.com>
2025-11-20 19:33:55 +0800

    format app.test.tsx

--- 2025-winter/intro/vitest-react-demo/tests/App.test.tsx
+++ 2025-winter/intro/vitest-react-demo/tests/App.test.tsx
@@ -1,13 +1,13 @@
-import { expect, test } from "vitest";
-import { render, screen } from "@testing-library/react";
-import App from "../src/App";
-import "@testing-library/jest-dom/vitest";
+import { expect, test } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import App from '../src/App';
+import '@testing-library/jest-dom/vitest';
 
-test("heading element should in the document", () => {
+test('heading element should in the document', () => {
   render(<App />);
 
-  const headingElement = screen.getByRole("heading");
-  const buttonElement = screen.getByRole("button");
+  const headingElement = screen.getByRole('heading');
+  const buttonElement = screen.getByRole('button');
 
   expect(headingElement).toBeInTheDocument();
   expect(buttonElement).toBeInTheDocument();
