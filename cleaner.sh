e93711f153fa8ca3b061d9ea6161eb87af46723a
alex <907785012@qq.com>
2025-11-24 17:38:14 +0800

    update cleaner script

--- cleaner.sh
+++ cleaner.sh
@@ -1,2 +1,5 @@
 # Find all node_modules directories and remove them
 find . -type d -name "node_modules" -exec rm -rf {} \;
+
+# Find all pnpm-lock.yaml files and remove them
+find . -type f -name "pnpm-lock.yaml" -exec rm -rf {} \;
