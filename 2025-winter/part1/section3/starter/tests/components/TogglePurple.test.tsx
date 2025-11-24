2a256c53c58bbfc156c4f2130b15713cfa2440e9
alex <907785012@qq.com>
2025-11-24 18:15:00 +0800

    add starter of section1-3

---
+++ 2025-winter/part1/section3/starter/tests/components/TogglePurple.test.tsx
@@ -0,0 +1,26 @@
+import { render, screen } from '@testing-library/react';
+import TogglePurple from '../../src/components/TogglePurple';
+
+describe('TogglePurple render test', () => {
+  const isPurple = false;
+  function setIsPurple(isPurple: boolean) {}
+
+  render(<TogglePurple isPurple={isPurple} setIsPurple={setIsPurple} />);
+
+  it('should render the checkbox and the checkbox should not be checked', () => {
+    const checkbox = screen.getByRole('checkbox');
+
+    // expect(element).matcher
+    expect(checkbox).toBeInTheDocument();
+    expect(checkbox).not.toBeChecked();
+  });
+
+  it('should render the label with purple text', () => {
+    // const label = screen.getByText(/purple/i);
+
+    screen.debug();
+
+    // expect(element).matcher;
+    // expect(label).toBeInTheDocument();
+  });
+});
