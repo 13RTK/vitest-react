ebacd253c8fffe0fffc83070a9ef37340eff156a
alex <907785012@qq.com>
2025-12-24 17:36:40 +0800

    remove useless example in part3

--- 2025-winter/part3/final/src/atoms/noteAtom.ts
+++
@@ -1,8 +0,0 @@
-import type { Note } from '@/types/Note';
-import { atom } from 'jotai';
-
-export const noteAtom = atom<Note>({
-  id: 999,
-  title: 'Example Title',
-  content: 'Example Content',
-});
--- 2025-winter/part3/final/src/hooks/useModel.ts
+++
@@ -1,9 +0,0 @@
-export function openModal(modalId: string) {
-  const modal = document.getElementById(modalId) as any;
-  modal.showModal();
-}
-
-export function closeModal(modalId: string) {
-  const modal = document.getElementById(modalId) as any;
-  modal.close();
-}
