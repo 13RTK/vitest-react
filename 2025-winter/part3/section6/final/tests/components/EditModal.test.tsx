27144d64aa2ab3e0a3b7339d297dd564d0c63404
alex <907785012@qq.com>
2025-12-31 14:59:29 +0800

    complete section3-6(form interaction)

--- 2025-winter/part3/section6/final/tests/components/EditModal.test.tsx
+++ 2025-winter/part3/section6/final/tests/components/EditModal.test.tsx
@@ -1,36 +1,38 @@
 import EditModal from '@/components/EditModal';
 import { openModal } from '@/hooks/useModel';
 import type { Note } from '@/types/Note';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { useAtomValue } from 'jotai';
 import { Toaster } from 'sonner';
 import { render } from 'vitest-browser-react';
+import { Wrapper } from '../provider';
+import { userEvent } from 'vitest/browser';
 
 describe('EditModal', () => {
-  const queryClient = new QueryClient();
-
   async function renderComponent() {
     return render(
-      <QueryClientProvider client={queryClient}>
+      <>
         <EditModal />
         <Toaster />
-      </QueryClientProvider>
+      </>,
+      {
+        wrapper: Wrapper,
+      }
     );
   }
 
+  vi.mock('jotai', async () => {
+    const originalModule = await vi.importActual('jotai');
+
+    return { ...originalModule, useAtomValue: vi.fn() };
+  });
+
+  const mockedNoteForAdd: Note = {
+    id: 999,
+    title: 'test title',
+    content: 'test content',
+  };
+
   describe('render test', () => {
-    vi.mock('jotai', async () => {
-      const originalModule = await vi.importActual('jotai');
-
-      return { ...originalModule, useAtomValue: vi.fn() };
-    });
-
-    const mockedNoteForAdd: Note = {
-      id: 999,
-      title: 'test title',
-      content: 'test content',
-    };
-
     it('should render the correct content for add form with default values', async () => {
       vi.mocked(useAtomValue).mockReturnValue(mockedNoteForAdd);
       const { getByLabelText } = await renderComponent();
@@ -43,5 +45,20 @@ describe('EditModal', () => {
       await expect.element(content).toHaveValue(mockedNoteForAdd.content);
     });
   });
-  // describe('user interaction', () => {});
+
+  describe('user interaction', () => {
+    it('should render the toaster after user click the add button', async () => {
+      vi.mocked(useAtomValue).mockReturnValue(mockedNoteForAdd);
+      const { getByRole, getByText } = await renderComponent();
+      openModal('my_modal_5');
+      const addButton = getByRole('button', {
+        name: /add/i,
+      });
+
+      await userEvent.click(addButton);
+      const toast = getByText(/created/i);
+
+      await expect.element(toast).toBeInTheDocument();
+    });
+  });
 });
